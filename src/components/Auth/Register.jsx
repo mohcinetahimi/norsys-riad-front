99954347c7a17e62f78b009a7259945f6c3f3c53
NOUSSAIBA <elboudalinoussaiba@gmail.com>
2024-08-26 10:47:08 +0100

    reservation form for others and for myself

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -7,6 +7,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css';
+import logo from '../../assets/logo.jpg';
 
 const schema = yup.object().shape({
   username: yup
@@ -84,9 +85,9 @@ const Register = () => {
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          alt="Your Company"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          className="mx-auto h-10 w-auto"
+          alt="Riad Reservation"
+          src= {logo}
+          className="mx-auto h-20 w-20"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Create your account
